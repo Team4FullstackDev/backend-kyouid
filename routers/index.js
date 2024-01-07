46fa4f4f974671ebc27b0773e5295f1d41e49c4b
EffenrilAgung <effenrilagung300@gmail.com>
2024-01-07 10:45:56 +0700

    code reset

--- routers/index.js
+++ routers/index.js
@@ -4,7 +4,5 @@ const router = express.Router();
 const userModel = require('../controllers/users');
 
 router.get('/', userModel.home);
-router.post('/users', userModel.createUser);
-router.get('/users', userModel.getAll);
 
 module.exports = router;
