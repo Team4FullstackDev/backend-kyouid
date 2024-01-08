a64bd7736aafb7689bf526df95a46327f38f4957
EffenrilAgung <effenrilagung300@gmail.com>
2024-01-08 12:41:09 +0700

    Fix model products

--- db/migrations/20240108053044-create-image-products.js
+++ db/migrations/20240108053044-create-image-products.js
@@ -1,34 +1,34 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Image_Products', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      thumbnail: {
-        type: Sequelize.STRING
-      },
-      image: {
-        type: Sequelize.STRING
-      },
-      productsId: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Image_Products');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Image_Products', {
+			id: {
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				allowNull: false,
+			},
+			thumbnail: {
+				type: Sequelize.STRING,
+			},
+			image: {
+				type: Sequelize.STRING,
+			},
+			productsId: {
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Image_Products');
+	},
 };
